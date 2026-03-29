9024b80e3f12bf39d9555abfd905cb1415d60074
Agamjeet Singh <agamjeet.singh24@imperial.ac.uk>
2026-03-29 21:15:18 +0100

    refactor: Fixed order of initialisation of fields in UIManager.h

--- include/gui/UIManager.h
+++ include/gui/UIManager.h
@@ -9,10 +9,10 @@
 
 class UIManager {
 public:
-    explicit UIManager(EventBus& event_bus): clickListener(Listener::make_listener<sf::Event::MouseButtonPressed>([this](const sf::Event::MouseButtonPressed& e) {
+    explicit UIManager(EventBus& event_bus): widget(std::make_unique<Widget>(std::make_unique<RectHitbox>(sf::Vector2f{0, 0}, sf::Vector2f{MAXFLOAT, MAXFLOAT}))),
+    clickListener(Listener::make_listener<sf::Event::MouseButtonPressed>([this](const sf::Event::MouseButtonPressed& e) {
         widget->onClick(e);
     }, event_bus)),
-    widget(std::make_unique<Widget>(std::make_unique<RectHitbox>(sf::Vector2f{0, 0}, sf::Vector2f{MAXFLOAT, MAXFLOAT}))),
     mouseMovementListener(Listener::make_listener<MousePosition>([this](const MousePosition& e) {
         widget->onHover(e);
     }, event_bus)) {}
