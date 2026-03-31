7b18e13ff7d9c9d1fa5a888cfc37239e71b0ce1b
Agamjeet Singh <agamjeet.singh24@imperial.ac.uk>
2026-03-31 01:21:46 +0100

    refactor: Removed extra callback abstraction and made ScheduledCallback better encapsulated

--- include/events/callbacks/ScheduledCallback.h
+++ include/events/callbacks/ScheduledCallback.h
@@ -28,18 +28,12 @@ struct ScheduledCallback {
     float timeRemaining;
     float spentTime = 0;
 
-    const std::function<void(ScheduledCallback*, float)> user_callback;
-
     ScheduledCallback(
        std::function<void(ScheduledCallback*, float)> cb,
        const float time,
        const bool repeat = false,
        const float interval = 0)
-    : timeRemaining(time), user_callback(std::move(cb)), callback([this](float deltaTime) {
-        if (!this->cancelled) {
-            this->user_callback(this, deltaTime);
-        }
-    }), interval(interval), repeat(repeat) {
+    : timeRemaining(time), user_callback(std::move(cb)), interval(interval), repeat(repeat) {
         assert(time >= 0);
         assert(interval >= 0);
     }
@@ -52,17 +46,74 @@ struct ScheduledCallback {
         return cancelled.load(std::memory_order_acquire);
     }
 
-    void call(float dt) const {
-        callback(dt);
+    void call(float dt) {
+        if (!this->cancelled) {
+            this->user_callback(this, dt);
+        }
+    }
+
+    float getInterval() const {
+        return interval;
+    }
+
+    bool isRepeating() const {
+        return repeat;
     }
 
 private:
     std::atomic_bool cancelled = false;
-    std::function<void(float)> callback;
 
-    const float interval; // interval = 0 would mean run once every frame, events run a maximum of once per frame
+    float interval; // interval = 0 would mean run once every frame, events run a maximum of once per frame
 
-    const bool repeat;
+    bool repeat;
+
+    std::function<void(ScheduledCallback*, float)> user_callback;
+
+public:
+
+    // 1. Copy Constructor
+    ScheduledCallback(const ScheduledCallback& other)
+        : timeRemaining(other.timeRemaining),
+          spentTime(other.spentTime),
+          interval(other.interval),
+          repeat(other.repeat),
+          user_callback(other.user_callback),
+          cancelled(other.cancelled.load(std::memory_order_acquire)) {}
+
+    // 2. Move Constructor
+    ScheduledCallback(ScheduledCallback&& other) noexcept
+        : timeRemaining(other.timeRemaining),
+          spentTime(other.spentTime),
+          interval(other.interval),
+          repeat(other.repeat),
+          user_callback(std::move(other.user_callback)),
+          cancelled(other.cancelled.load(std::memory_order_acquire)) {}
+
+    // 3. Copy Assignment Operator
+    ScheduledCallback& operator=(const ScheduledCallback& other) {
+        if (this != &other) {
+            timeRemaining = other.timeRemaining;
+            spentTime = other.spentTime;
+            interval = other.interval;
+            repeat = other.repeat;
+            user_callback = other.user_callback;
+            cancelled.store(other.cancelled.load(std::memory_order_acquire), std::memory_order_release);
+        }
+        return *this;
+    }
+
+    // 4. Move Assignment Operator
+    ScheduledCallback& operator=(ScheduledCallback&& other) noexcept {
+        if (this != &other) {
+            timeRemaining = other.timeRemaining;
+            spentTime = other.spentTime;
+            interval = other.interval;
+            repeat = other.repeat;
+            user_callback = std::move(other.user_callback);
+            cancelled.store(other.cancelled.load(std::memory_order_acquire), std::memory_order_release);
+        }
+        return *this;
+    }
 };
 
 #endif //ROOT_BOARD_GAME_FRONTEND_SCHEDULEDCALLBACK_H
